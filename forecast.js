5f9d4b99277ba620ce1a884d35a535aeb1e951ed
kabir ajibade <kabirajibade@yahoo.com>
2025-04-25 20:37:59 +0100

    Done with forecast for some cities

--- forecast.js
+++ forecast.js
@@ -1,152 +1,68 @@
-// Damaturu 
+// Function to fetch weather data for each city 
 function showCaption(button) {
-    let town=button.innerText;
+    let town = button.innerText;
     alert("Weather for: " + button.innerText);
-//   }
+    document.getElementById('Town').innerHTML='';
+document.getElementById('Town').innerHTML=town;
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=${town}&aqi=yes`)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        let forecasts = [];
+        forecasts[0] = res.forecast.forecastday[0];
 
-fetch(`https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=${town}&aqi=yes`)
-.then(res=> res.json())
-.then(res =>{
-console.log(res);
-    let forecasts=[];
-    forecasts[0]=res.forecast.forecastday[0];
-    // console.log(forecasts[0].hour[0].condition)
-    let div4=document.getElementById('div4');
-    div4.setAttribute('class','w-100 row ps-4');
-    // looping through arrays to fetch data 
-    for(let i=0; i<24; i++){
+        let div4 = document.getElementById('div4');
         
-        let wForeCast=forecasts[0].hour[i];
-        console.log(JSON.stringify(wForeCast))
-        let condition=wForeCast.condition.text;
-        let img=wForeCast.condition.icon;;
-        let city=wForeCast.city;
-        let time=wForeCast.time;
-        let tempC=wForeCast.temp_c;
-        let humidity=wForeCast.humidity;
+        // Clear existing content before appending new data
+        div4.innerHTML = ''; // This will remove any existing content from div4
 
-        let div1=document.createElement('div');
-        div1.setAttribute('class', 'col-md-4')
-        // creating elements 
-        let hTag=document.createElement('h3');
-        let pTagTime=document.createElement('p');
-        let pTagTemp=document.createElement('p');
-        let pTagHum=document.createElement('p');
-        let imgTag=document.createElement('img');
-
-        // setting values 
-        hTag.innerHTML=condition;
-        pTagTime.innerHTML=time;
-        pTagTemp.innerHTML="Temperature is "+ tempC +" Celsius";
-        pTagHum.innerHTML="Humidity is " +humidity;
-        imgTag.src=img;
-
-        // setting the attributes 
-        hTag.setAttribute('class', 'card-title');
-        pTagTime.setAttribute('class', 'card-text');
-        pTagTemp.setAttribute('class', 'card-text');
-        pTagHum.setAttribute('class', 'card-text')
-        imgTag.setAttribute('class','img');
+        div4.setAttribute('class', 'w-100 row ps-4');
         
-       
-        // appending the elements 
-        div1.append(imgTag);
-        div1.append(hTag);
-        div1.append(pTagTime);
-        div1.append(pTagTemp);
-        div1.append(pTagHum);
-        div4.append(div1);
-    }
-        
-})
+        // Loop through arrays to fetch data
+        for (let i = 0; i < 24; i++) {
+            let wForeCast = forecasts[0].hour[i];
+            console.log(JSON.stringify(wForeCast));
 
+            let condition = wForeCast.condition.text;
+            let img = wForeCast.condition.icon;
+            let city = wForeCast.city;
+            let time = wForeCast.time;
+            let tempC = wForeCast.temp_c;
+            let humidity = wForeCast.humidity;
+
+            let div1 = document.createElement('div');
+            div1.setAttribute('class', 'col-md-4');
+
+            // Creating elements
+            let hTag = document.createElement('h3');
+            let pTagTime = document.createElement('p');
+            let pTagTemp = document.createElement('p');
+            let pTagHum = document.createElement('p');
+            let imgTag = document.createElement('img');
+
+            // Setting values
+            hTag.innerHTML = condition;
+            pTagTime.innerHTML = time;
+            pTagTemp.innerHTML = "Temperature is " + tempC + " Celsius";
+            pTagHum.innerHTML = "Humidity is " + humidity;
+            imgTag.src = img;
+
+            // Setting the attributes
+            hTag.setAttribute('class', 'card-title');
+            pTagTime.setAttribute('class', 'card-text');
+            pTagTemp.setAttribute('class', 'card-text');
+            pTagHum.setAttribute('class', 'card-text');
+            imgTag.setAttribute('class', 'img');
+
+            // Appending the elements
+            div1.append(imgTag);
+            div1.append(hTag);
+            div1.append(pTagTime);
+            div1.append(pTagTemp);
+            div1.append(pTagHum);
+            div4.append(div1);
+        }
+    });
 }
-alert("Hey the Listener, This is a real thing")
-// Abuja
-// fetch('https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=Abuja&aqi=no')
-// .then(res=> res.json())
-// .then(res =>{
-//     console.log(res);
-//     let condition1=res.current.condition.text;
-//     let img1=res.current.condition.icon;
-//     let city1=res.location.name;
-//     let country1=res.location.country;
-//     let tempC1=res.current.feelslike_c;
-//     let tempF1=res.current.feelslike_f;
-//     document.getElementById('condition1').innerHTML=condition1;
-//     document.getElementById('city1').innerHTML=city1;
-//     document.getElementById('img1').src=img1;
-//     document.getElementById('tempC1').innerHTML=tempC1;
-//     document.getElementById('tempF1').innerHTML=tempF1;
-// })
 
-// Lagos 
-// fetch('https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=Lagos&aqi=no')
-// .then(res=> res.json())
-// .then(res =>{
-//     console.log(res)
-//     let condition2=res.current.condition.text;
-//     let img2=res.current.condition.icon;
-//     let city2=res.location.name;
-//     let country2=res.location.country;
-//     let tempC2=res.current.feelslike_c;
-//     let tempF2=res.current.feelslike_f;
-//     document.getElementById('condition2').innerHTML=condition2;
-//     document.getElementById('city2').innerHTML=city2;
-//     document.getElementById('img2').src=img2;
-//     document.getElementById('tempC2').innerHTML=tempC2;
-//     document.getElementById('tempF2').innerHTML=tempF2;
-// })
-
-// Lagos 
-// fetch('https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=Oshogbo&aqi=yes')
-// .then(res=> res.json())
-// .then(res =>{
-//     console.log(res)
-//     let condition3=res.current.condition.text;
-//     let img3=res.current.condition.icon;
-//     let city3=res.location.name;
-//     // let country2=res.location.country;
-//     let tempC3=res.current.feelslike_c;
-//     let tempF3=res.current.feelslike_f;
-//     document.getElementById('condition3').innerHTML=condition3;
-//     document.getElementById('city3').innerHTML=city3;
-//     document.getElementById('img3').src=img3;
-//     document.getElementById('tempC3').innerHTML=tempC3;
-//     document.getElementById('tempF3').innerHTML=tempF3;
-// })
-
-// Kano 
-// fetch('https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=Yola&aqi=yes')
-// .then(res=> res.json())
-// .then(res =>{
-//     console.log(res)
-//     let condition4=res.current.condition.text;
-//     let img4=res.current.condition.icon;
-//     let city4=res.location.name;
-//     // let country2=res.location.country;
-//     let tempC4=res.current.feelslike_c;
-//     let tempF4=res.current.feelslike_f;
-//     document.getElementById('condition4').innerHTML=condition4;
-//     document.getElementById('city4').innerHTML=city4;
-//     document.getElementById('img4').src=img4;
-//     document.getElementById('tempC4').innerHTML=tempC4;
-//     document.getElementById('tempF4').innerHTML=tempF4;
-// })
-
-// Portharcourt 
-// fetch('https://api.weatherapi.com/v1/forecast.json?key=c900f9d4689c4f91acf105519252004&q=Port Harcourt&aqi=yes')
-// .then(res=> res.json())
-// .then(res =>{
-//     console.log(res)
-//     let condition5=res.current.condition.text;
-//     let img5=res.current.condition.icon;
-//     let city5=res.location.name;
-//     let tempC5=res.current.feelslike_c;
-//     let tempF5=res.current.feelslike_f;
-//     document.getElementById('condition5').innerHTML=condition5;
-//     document.getElementById('city5').innerHTML=city5;
-//     document.getElementById('img5').src=img5;
-//     document.getElementById('tempC5').innerHTML=tempC5;
-//     document.getElementById('tempF5').innerHTML=tempF5;
-// })
+alert("Hey there you Listener, This is a real thing");
